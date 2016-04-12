11258253c3a2211f1b072d795f10a627bb8468f0
lijian <827992983@qq.com>
2016-04-12 22:06:01 +0800

    finish f5 refresh with ajax

--- admin/static/js/admin.js
+++ admin/static/js/admin.js
@@ -23,70 +23,94 @@ function adminOnload() {
         xhr.send();
     }
 
-    document.getElementById('chagepassword').onclick = function () {
-        $.ajax({
-                async: false,
-                url: "static/html/changepwd.html",
-                dataType: "text",
-                success: function (result) {
-                    document.getElementById("mainsession").innerHTML = result;
+    document.getElementById('chagepassword').onclick = changePassword;
+    document.getElementById("help").onclick = adminHelp;
+    document.getElementById("usermgmt").onclick = userMgmt;
 
-//                    $(".input_change_pwd").blur(function () {
-//
-//                        }
-//                    )
+    var currUrl = window.location.toString();
+    var index = currUrl.indexOf("#", 0);
+    var action = currUrl.substring(index, currUrl.length);
+    if (action == "#changepwd") {
+        changePassword();
+    } else if (action == "#help") {
+        adminHelp();
+    } else if (action == "#user") {
+        userMgmt();
+    } else {
 
-                    $("#submit_chang_pwd").click(function () {
-                            var old_pwd = document.getElementsByClassName("input_change_pwd")[0].value;
-                            var new_pwd = document.getElementsByClassName("input_change_pwd")[1].value;
-                            var confirm_pwd = document.getElementsByClassName("input_change_pwd")[2].value;
-                            if (old_pwd.length > 0 && new_pwd.length > 0 && confirm_pwd.length > 0) {
-                                if (new_pwd != confirm_pwd) {
-                                    alert("新密码不一致，请重新输入！");
-                                } else {
-
-                                    var reg = /^[0-9a-zA-Z]+$/;
-                                    if (new_pwd.length < 6 || confirm_pwd.length < 6 || !reg.test(new_pwd) || !reg.test(confirm_pwd)) {
-                                        alert("密码不少于6位,由字母、数字组成！");
-                                    } else {
-                                        jsondata = new Object();
-                                        jsondata.new = new_pwd;
-                                        jsondata.confirm = confirm_pwd;
-                                        jsondata.old = old_pwd;
-                                        $.ajax({
-                                            url: "/changepwd",
-                                            type: "POST",
-//                                            data: $('#form_change_pwd').serialize(),
-                                            data: JSON.stringify(jsondata),
-                                            dataType: "json",
-                                            beforeSend: function (request) {
-                                                request.setRequestHeader('X-CSRFToken', getCookie("csrftoken"))
-                                            },
-                                            success: function (result) {
-                                                alert(result.status);
-                                            }
-                                        });
-                                    }
-                                }
-                            }
-                        }
-                    )
-                }
-            }
-        )
-        ;
     }
+}
 
-    document.getElementById("help").onclick = function () {
-        $.ajax({
+function userMgmt() {
+    $.ajax({
+        async: false,
+        url: "static/html/usermgmt.html",
+        dataType: "text",
+        success: function (result) {
+            document.getElementById("mainsession").innerHTML = result;
+        }
+    });
+}
+
+function changePassword() {
+    $.ajax({
             async: false,
-            url: "static/html/adminhelp.html",
+            url: "static/html/changepwd.html",
             dataType: "text",
             success: function (result) {
                 document.getElementById("mainsession").innerHTML = result;
+
+                $("#submit_chang_pwd").click(function () {
+                        var old_pwd = document.getElementsByClassName("input_change_pwd")[0].value;
+                        var new_pwd = document.getElementsByClassName("input_change_pwd")[1].value;
+                        var confirm_pwd = document.getElementsByClassName("input_change_pwd")[2].value;
+                        if (old_pwd.length > 0 && new_pwd.length > 0 && confirm_pwd.length > 0) {
+                            if (new_pwd != confirm_pwd) {
+                                alert("新密码不一致，请重新输入！");
+                            } else {
+
+                                var reg = /^[0-9a-zA-Z]+$/;
+                                if (new_pwd.length < 6 || confirm_pwd.length < 6 || !reg.test(new_pwd) || !reg.test(confirm_pwd)) {
+                                    alert("密码不少于6位,由字母、数字组成！");
+                                } else {
+                                    jsondata = new Object();
+                                    jsondata.new = new_pwd;
+                                    jsondata.confirm = confirm_pwd;
+                                    jsondata.old = old_pwd;
+                                    $.ajax({
+                                        url: "/changepwd",
+                                        type: "POST",
+//                                            data: $('#form_change_pwd').serialize(),
+                                        data: JSON.stringify(jsondata),
+                                        dataType: "json",
+                                        beforeSend: function (request) {
+                                            request.setRequestHeader('X-CSRFToken', getCookie("csrftoken"))
+                                        },
+                                        success: function (result) {
+                                            if (result.status == 0) {
+                                                alert("密码修改成功！")
+                                            } else {
+                                                alert("密码修改失败！")
+                                            }
+                                        }
+                                    });
+                                }
+                            }
+                        }
+                    }
+                )
             }
-        });
-    }
+        }
+    );
+}
 
-
+function adminHelp() {
+    $.ajax({
+        async: false,
+        url: "static/html/adminhelp.html",
+        dataType: "text",
+        success: function (result) {
+            document.getElementById("mainsession").innerHTML = result;
+        }
+    });
 }
