dc4cee565d2c23ff38b5d8233fb302e83af3f782
lijian <827992983@qq.com>
2016-04-16 17:59:41 +0800

    get os version

--- config/sysconfig.py
+++ config/sysconfig.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # author:lijian
-#date: 2016
+# date: 2016
 #Copyright: free
 
 def getOsVersion():
-     return "helloworld!"
+    ret = ""
+    try:
+        fd = open("/etc/redhat-release")
+        ret = fd.readline()
+        fd.close()
+    except:
+        pass
+
+    return ret
